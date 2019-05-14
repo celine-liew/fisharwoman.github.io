4fbf7effb935e2c9c10b791a48c3da71a462de41
Celine <celineliew.hx@gmail.com>
2019-05-13 23:12:58 -0700

    remove generated files

--- packages/landing-gatsby/public/styles-ce507721fd04ddd7bbcf.js
+++
@@ -1,9 +0,0 @@
-(window.webpackJsonp = window.webpackJsonp || []).push([
-  [3],
-  {
-    358: function(n, o, w) {},
-    458: function(n, o, w) {},
-    487: function(n, o, w) {},
-  },
-]);
-//# sourceMappingURL=styles-ce507721fd04ddd7bbcf.js.map
--- packages/landing-gatsby/public/styles-d59f9588a33a99d39096.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{358:function(n,o,w){},460:function(n,o,w){},489:function(n,o,w){}}]);
-//# sourceMappingURL=styles-d59f9588a33a99d39096.js.map
